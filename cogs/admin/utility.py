b575eb8643c0f905d02dc164251abaa471f26c10
Jason <110392848+JasonZhaoExp@users.noreply.github.com>
2024-11-29 10:27:34 +0000

    fixed rq

--- cogs/admin/utility.py
+++ cogs/admin/utility.py
@@ -13,7 +13,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-s
 
 from discord.ext import commands
 from utils.helpers import bot_manager
